9ba7d87bbc4aa4a3984489cd21ec49356f8e04fc
François Chabot <fchabot@skynet.be>
2020-01-06 17:18:01 +0100

    review build script and package creation

--- build/build.ps1
+++ build/build.ps1
@@ -23,16 +23,13 @@ Clear-Host
 # construct a Version object to ensure arguments are valid
 $version = New-Object -TypeName System.Version -ArgumentList $Major, $Minor, $Build, $Revision
 
+# build and package solution
+#https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg
+dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
+dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true
+
 # generate build.local.ps1 script file that allows to redo a build and package locally wihtout altering the build version number
 $path = Split-Path $script:MyInvocation.MyCommand.Path
 @"
 & $(Join-Path $path build.ps1) -Major $($version.Major) -Minor $($version.Minor) -Build $($version.Build) -Revision $($version.Revision)
 "@ > $(Join-Path $path build.local.ps1)
-
-# build and package solution
-dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-#https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg
-dotnet pack -p:Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;IncludeSymbols=true`;SymbolPackageFormat=snupkg
-#https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages
-# dotnet pack -p:Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision) --include-source
