6b880a844bad930a4be3bf6914c157d3cb2b562a
François Chabot <fchabot@skynet.be>
2020-01-26 07:12:49 +0100

    generate XML documentation

--- build/build.ps1
+++ build/build.ps1
@@ -26,7 +26,7 @@ $version = New-Object -TypeName System.Version -ArgumentList $Major, $Minor, $Bu
 # build and package solution
 #https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg
 dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true
+dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true`;NoWarn=1591
 
 # generate build.local.ps1 script file that allows to redo a build and package locally wihtout altering the build version number
 $path = Split-Path $script:MyInvocation.MyCommand.Path
